a9238c1714dc091caf4eb637fee0520838f2441c
Oguzhan Cirpan <ocirpan920@gmail.com>
2025-03-27 20:13:48 +0100

    Probe

---
+++ FirstProjekt/Probe.swift
@@ -0,0 +1,18 @@
+//
+//  Probe.swift
+//  FirstProjekt
+//
+//  Created by Oguzhan Cirpan on 27.03.25.
+//
+
+import SwiftUI
+
+struct Probe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Probe()
+}
